da2533573f9fe855358843acbdea8a6e9f75f813
Mohammed Niyas <mailforniyas@gmail.com>
2022-06-30 16:45:41 +0000

    Update LR

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ for epoch in range(args.epochs):
         save_ckp(checkpoint, is_best, checkpoint_path, best_model_path)
 
     print('Epoch: {:03d}, LR: {:.07d}, Train MAE: {:.7f}, Validation MAE: {:.7f}, '
-          'Test MAE: {:.7f}'.format(epoch+1, scheduler_warmup.get_lr(), train_loss, val_loss, test_loss))
+          'Test MAE: {:.7f}'.format(epoch+1, optimizer.param_groups[0]['lr'], train_loss, val_loss, test_loss))
 
 print('===================================================================================')
 print('Best Epoch:', best_epoch)
